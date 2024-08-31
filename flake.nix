67cba5088324d036c8fdc233035ebd1a27a75bae
Oliver Ni <oliverni@berkeley.edu>
2024-08-31 12:31:45 -0700

    feat: add vault secret pushing helper

--- flake.nix
+++ flake.nix
@@ -44,6 +44,48 @@
           inherit openApiSpec;
           modules = kubernetesModules ++ kubernetesExtraModules;
         };
+
+        # This is used for the `push-vault-secrets` app
+        # It's a bit of a hack, but it works for now :)
+        __raw-secrets-to-push = builtins.groupBy
+          (obj: obj.metadata.namespace)
+          (builtins.filter
+            (obj: obj.apiVersion == "v1" && obj.kind == "Secret")
+            (transpire.lib.${system}.evalModules {
+              inherit openApiSpec;
+              modules = kubernetesModules;
+            }).config.build.objects);
+      });
+
+      apps = forAllSystems (system: pkgs: {
+        update-kube-openapi = {
+          type = "app";
+          program = toString (pkgs.writers.writeBash "update-kube-openapi" ''
+            ${pkgs.kubectl}/bin/kubectl get --raw /openapi/v2 > kube-openapi.json
+          '');
+        };
+
+        push-vault-secrets = {
+          type = "app";
+          program = toString (pkgs.writers.writeBash "push-vault-secrets" ''
+            set -e
+            if [[ $# -eq 0 ]] ; then
+              echo 'Usage: push-vault-secrets <namespace>'
+              exit 1
+            fi
+            nix eval --json --impure ".#__raw-secrets-to-push.$1" \
+            | ${pkgs.jq}/bin/jq -r -c '.[] |
+                .metadata.namespace + "/" + .metadata.name,
+                (.data // {} | .[] |= @base64d) + (.stringData // {})
+              ' \
+            | while read -r path; read -r data; do
+              read -r -p "Push $path? [y/N] " choice <&2
+              if [[ $choice =~ ^[Yy] ]] ; then
+                ${pkgs.vault-bin}/bin/vault kv put -mount=hfym-ds "$path" - <<< "$data" > /dev/null
+              fi
+            done
+          '');
+        };
       });
     };
 }
