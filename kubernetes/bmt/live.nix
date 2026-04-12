eaa0f8b312ed31b5c74c1778f3cb5dd326430f93
Oliver Ni <oliver.ni@cognition.ai>
2026-04-12 03:20:33 -0700

    revert: remove command override in live deployment (fixed in Dockerfile)

--- kubernetes/bmt/live.nix
+++ kubernetes/bmt/live.nix
@@ -26,7 +26,6 @@
               name = "data";
               mountPath = "/app/data";
             }];
-            command = [ "/app/server" ];
             workingDir = "/app/data";
             resources = {
               limits = { memory = "512Mi"; };
